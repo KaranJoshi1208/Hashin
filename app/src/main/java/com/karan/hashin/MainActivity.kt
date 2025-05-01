2ed58978e2ed0d2668be16fdf42f3f4010c0ecde
Karan Joshi <karanjoshi1208@gmail.com>
2025-05-01 18:11:54 +0530

    ✔️Ln Screen UI

--- app/src/main/java/com/karan/hashin/MainActivity.kt
+++ app/src/main/java/com/karan/hashin/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.karan.hashin.screens.AuthScreen
 import com.karan.hashin.ui.theme.HashinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HashinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AuthScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HashinTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/karan/hashin/screens/Auth.kt
@@ -0,0 +1,187 @@
+package com.karan.hashin.screens
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import com.karan.hashin.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.karan.hashin.ui.theme.HashinTheme
+
+@Composable
+fun AuthScreen(modifier: Modifier = Modifier) {
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(top = 88.dp)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "#in",
+                fontSize = 68.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            )
+            Text(
+                text = "Hash Your Secrets",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.W300,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            )
+
+            SignIn(Modifier.padding(top = 92.dp))
+        }
+    }
+}
+
+@Composable
+fun SignIn(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var pass by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = pass,
+            onValueChange = { pass = it },
+            label = { Text("Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = { /* Handle click */ },
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .width(144.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF673AB7),
+                contentColor = Color.White
+            ),
+            shape = RoundedCornerShape(size = 20.dp)
+        ) {
+            Text("Login")
+        }
+
+//        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+        ) {
+            Spacer(Modifier.weight(1f).height(0.5.dp).background(color = Color.Black))
+            Text("  OR  ", textAlign = TextAlign.Center)
+            Spacer(Modifier.weight(1f).height(0.5.dp).background(color = Color.Black))
+        }
+
+        OutlinedButton(
+            onClick = { /* TODO(handle Google login) */ },
+            modifier = Modifier
+//                .fillMaxWidth()
+                .padding(top = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "Google",
+                modifier = Modifier
+                    .size(16.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Continue with Google")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+                .padding(top = 32.dp)
+        ) {
+
+            Text("New to #in ? ", textAlign = TextAlign.Center)
+            Text(
+                text = "Sign up!",
+                textAlign = TextAlign.Center,
+                color = Color.Blue,
+                textDecoration = TextDecoration.Underline
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun AuthPreview() {
+    HashinTheme {
+        AuthScreen()
+    }
+}
