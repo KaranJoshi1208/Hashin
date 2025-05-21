d70737927d587f50d45c70d7235ba523e811c889
Karan Joshi <karanjoshi1208@gmail.com>
2025-05-21 21:30:11 +0530

    room...

--- app/src/main/java/com/karan/hashin/model/local/DAO.kt
+++ app/src/main/java/com/karan/hashin/model/local/DAO.kt
@@ -1,4 +1,21 @@
 package com.karan.hashin.model.local
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface DAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addPasskey(passKey: PassKey)
+
+    @Query("SELECT * FROM PASSKEY")
+    fun getAll(): Flow<List<PassKey>>
+
+    @Delete
+    suspend fun delete(passKey: PassKey)
 }
---
+++ app/src/main/java/com/karan/hashin/model/local/Room.kt
@@ -0,0 +1,23 @@
+package com.karan.hashin.model.local
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+abstract class Database : RoomDatabase() {
+
+    abstract fun dao(): DAO
+
+    companion object {
+        @Volatile private var INSTANCE: Database? = null
+
+        fun getDatabase(context: Context): Database {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(context, Database::class.java, "app_database")
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+}
